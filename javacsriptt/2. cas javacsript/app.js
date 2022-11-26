9fdbd661512c10fef3bdc3ddad3788b029d9d50a
Ammar <mustaficammar0@gmail.com>
2022-11-26 14:37:27 +0100

    Add js file

---
+++ javacsriptt/2. cas javacsript/app.js
@@ -0,0 +1,72 @@
+ime = "Ammar";
+console.log(ime);
+ime = "Ammar2";
+console.log(ime);
+
+prviBroj = 10;
+console.log(prviBroj);
+
+prviBroj = prviBroj + 1; // prvi nacin
+console.log(prviBroj);
+
+prviBroj++; // drugi nacin
+console.log(prviBroj);
+
+prviBroj--;
+console.log(prviBroj);
+
+prviBroj+=10;
+console.log(prviBroj);
+
+prviBroj-=10;
+console.log(prviBroj);
+
+prviBroj*=10;
+console.log(prviBroj);
+
+prviBroj/=10;
+console.log(prviBroj);
+
+a = 10;
+if(a > 0) {
+    console.log("Broj je pozitivan");
+}
+else {
+    console.log("Broj je neagitvan");
+}
+
+ime = "Ammar";
+if (ime === "Ammar") {
+    console.log("Zdravo, Ammare!");
+} 
+else {
+    console.log("Ne znam ko si ti..");
+}
+
+console.log("----------ZADATAK 1----------");
+// zadatak 1.
+broj = -1;
+if(broj > 0){
+    console.log ("Broj je pozitivan");
+} 
+
+else if(broj = 0){
+    console.log("Broj je nula.");
+}
+
+else {
+    console.log("Broj je negativan.");
+}
+
+
+//zadatak 2
+console.log("----------ZADATAK 2----------");
+broj1 = 10; 
+broj2 = 5;
+if(broj1 == broj2){
+    console.log("Brojevi su jednaki.");
+} 
+else {
+    console.log("Brojevi nisu jednaki");
+}
+
