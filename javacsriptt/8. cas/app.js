d91819b9a2e05df6ebe23bf430acbbe786944072
Ammar <mustaficammar0@gmail.com>
2022-12-13 20:41:43 +0100

    Add files

---
+++ javacsriptt/8. cas/app.js
@@ -0,0 +1,66 @@
+// Uneti string i proveriti da li je on prazan, ako je prazan da ispisemo string je prazan, ako nije onda da ispisemo koliko ima karaktera
+nekitekst = prompt("Unesite neki string:");
+if(nekitekst.length == 0) {
+    console.log("String je prazan.");
+} else {
+    console.log("Duzina stringa je:",nekitekst.length);
+}
+
+
+// Uneti string koji sadrzi mala i velika slova, mala slova pretvoriti u velika, a velika u mala
+nesto = prompt("Unesite neki string:");
+novistring = "";
+for(i=0;i<nesto.length;i++) {
+    if(nesto[i] === nesto[i].toUpperCase()){
+        novistring+=nesto[i].toLowerCase();
+    }
+    else {
+        novistring+=nesto[i].toUpperCase();
+    }
+}
+console.log(novistring);
+
+
+// N I Z O V I 
+
+cars = [ "BMW", "Audi", "Mercedes" ];
+console.log(cars[0]);
+
+// metoda za dodavanje elemenata u niz
+cars.push("Golf");
+console.log(cars);
+
+// metoda za uklanjanje poslednjeg elementa iz niza
+cars.pop();
+console.log(cars);
+
+// ispisivanje svih elemenata niza
+for(i=0;i<cars.length;i++) {
+    console.log(cars[i]);
+}
+
+
+// 1. Napraviti duzi niz, i ispisati obrnutim redosledom
+console.log("-----ZADATAK 1-----");
+newCars = [];
+cars2 = [ "Nissan", "Porsche", "Lamborghini", "Fiat", "Ford", "Dodge", "Hyundai"];
+for(i=cars2.length-1;i>=0;i--){
+    newCars.push(cars2[i]);
+}
+console.log(newCars);
+
+// 2. Naci najveci i najmanji element niza
+niz = ["5", "6", "9", "2", "1"];
+max = niz[0];
+min = niz[0];
+for(i=0;i<niz.length;i++){
+    if(niz[i] > max) {
+        max = niz[i];
+    }
+    
+    if (niz[i] < min) {
+        min = niz[i];
+    }
+}
+console.log("Najveci element niza je:",max);
+console.log("Najmanji element niza je:",min);
